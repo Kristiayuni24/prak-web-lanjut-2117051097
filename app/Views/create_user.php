eacc4f45280ad997fb114d64b2dbf683046482a2
Kristiayuni24 <92391784+Kristiayuni24@users.noreply.github.com>
2023-09-24 16:42:55 +0700

    form-controller

--- app/Views/create_user.php
+++ app/Views/create_user.php
@@ -3,25 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form Bootstrap</title>
+
+    <!-- Tambahkan link ke Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- Tambahkan gaya CSS khusus di sini -->
+    <style>
+        body {
+            background-color: #d3d3d3;
+        }
+
+        .container {
+            background-color: #ffffff;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+        }
+
+        .form-group {
+            margin-bottom: 20px;
+        }
+
+        label {
+            font-weight: bold;
+        }
+
+        input.form-control {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
+
+        button.btn-primary {
+            background-color: #007bff;
+            border: none;
+        }
+
+        button.btn-primary:hover {
+            background-color: #0056b3;
+        }
+    </style>
 </head>
 <body>
-    <form action="<?=base_url('user/store')?>" method="post">
-    <table>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><input type="text" name="nama"></td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><input type="text" name="npm"></td>
-        </tr>
-        <tr>
-            <td><input type="submit" value="Simpan"></td>
-        </tr>
-    </table>
-    </form>
+    <div class="container mt-5">
+        <form action="<?=base_url('user/store')?>" method="post">
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan nama">
+            </div>
+            <div class="form-group">
+                <label for="kelas">Kelas</label>
+                <input type="text" class="form-control" id="kelas" name="kelas" placeholder="Masukkan kelas">
+            </div>
+            <div class="form-group">
+                <label for="npm">NPM</label>
+                <input type="text" class="form-control" id="npm" name="npm" placeholder="Masukkan NPM">
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat</label>
+                <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Masukkan alamat">
+            </div>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
+    </div>
 </body>
 </html>
--- app/Views/profile.php
+++ app/Views/profile.php
@@ -1,29 +1,76 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="id">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #d3d3d3; /* Warna latar belakang */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+            margin: 0;
+        }
+        .card {
+            border: none;
+            border-radius: 5px;
+            background-color: #ffc0cb; /* Warna card */
+            color: #fff; /* Warna teks pada card */
+            padding: 20px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3); /* Efek bayangan */
+        }
+        img {
+            display: block;
+            margin: 0 auto;
+            width: 250px;
+            height: 250px;
+            object-fit: cover;
+            object-position: center;
+            border-radius: 50%;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3); /* Efek bayangan */
+        }
+        table {
+            margin: 0 auto;
+            font-size: 20px;
+            line-height: 1.25;
+            max-width: 300px;
+        }
+        td {
+            padding: 8px 12px;
+        }
+    </style>
 </head>
 <body>
-    <form action="<?=base_url('user/store')?>" method="post">
-    <table>
-        <tr aria-colspan="3">
-            <td>
-                <img src="<?=base_url('/assets/img/ayuni.jpeg')?>" size="200" height="200">
-            </td>
-        </tr>
-        <tr>
-            <td>Nama</td>
-            <td>:</td>
-            <td><?=$nama?></td>
-        </tr>
-        <tr>
-            <td>NPM</td>
-            <td>:</td>
-            <td><?=$npm?></td>
-        </tr>
-    </table>
-    </form>
+<div class="card text-center">
+    <img src="<?php echo base_url('ayubi.png'); ?>" class="card-img-top rounded-circle mx-auto d-block" alt="...">
+    <div class="card-body">
+        <table class="table">
+            <tr>
+                <td>Nama</td>
+                <td>:</td>
+                <td><?= $nama ?></td>
+            </tr>
+            <tr>
+                <td>Kelas</td>
+                <td>:</td>
+                <td><?= $kelas ?></td>
+            </tr>
+            <tr>
+                <td>NPM</td>
+                <td>:</td>
+                <td><?= $npm ?></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td><?= $alamat ?></td>
+            </tr>
+        </table>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
